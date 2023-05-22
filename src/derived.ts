9341c06aae2ada873091602d2c7c74093cfe4873
Nikita Galaiko <nikita@galaiko.rocks>
2023-05-22 20:01:36 +0200

    wrap valilla derived to avoid mistakes

--- src/derived.ts
+++ src/derived.ts
@@ -1,5 +1,6 @@
-import { readable, type Readable } from 'svelte/store';
+import { derived, type Readable } from 'svelte/store';
 import type { Loadable, Loaded } from './types';
+import { Loading } from '.';
 
 type Stores<T> =
 	| Readable<Loadable<T>>
@@ -12,44 +13,31 @@ type StoresValues<T> = T extends Readable<Loadable<infer U>>
 			[K in keyof T]: T[K] extends Readable<Loadable<infer U>> ? U : never;
 	  };
 
+const single = <U, T>(store: Readable<Loadable<U>>, fn: (value: U) => T): Readable<Loadable<T>> =>
+	derived(store, (value) =>
+		value.isLoading
+			? ({ isLoading: true } as Loading)
+			: ({ isLoading: false, value: fn(value.value) } as Loaded<T>)
+	);
+
+const isLoaded = <U>(value: Loadable<U>): value is Loaded<U> => !value.isLoading;
+
+const array = <U, T>(
+	stores: Array<Readable<Loadable<U>>>,
+	fn: (values: Array<U>) => T
+): Readable<Loadable<T>> =>
+	derived(stores, (values) => {
+		const loaded = values.filter(isLoaded);
+		return loaded.length === values.length
+			? ({ isLoading: false, value: fn(loaded.map(({ value }) => value)) } as Loaded<T>)
+			: ({ isLoading: true } as Loading);
+	});
+
 // derived works similar to how it works in vanilla svelte.
 // it can be used to create an asynchronously populated store from other asynchronously populated stores.
 // the store will be in a loading state until all the dependencies are resolved.
 export default <S extends Stores<any>, T>(
 	stores: S,
 	fn: (values: StoresValues<S>) => T
-): Readable<Loadable<T>> => {
-	const single = !Array.isArray(stores);
-	const stores_array: Array<Readable<Loadable<T>>> = single
-		? [stores as Readable<Loadable<T>>]
-		: (stores as Array<Readable<Loadable<T>>>);
-
-	return readable<Loadable<T>>({ isLoading: true }, (set) => {
-		let loaded = 0;
-		const values = new Array<Loaded<T>>(stores_array.length);
-		const unsubscribers = new Array(stores_array.length);
-
-		const update = () => {
-			if (loaded === values.length) {
-				set({
-					isLoading: false,
-					value: fn(single ? values[0].value : (values.map(({ value }) => value) as any))
-				});
-			}
-		};
-
-		stores_array.forEach((store, i) => {
-			unsubscribers[i] = store.subscribe((value) => {
-				if (!value.isLoading) {
-					values[i] = value;
-					loaded += 1;
-					update();
-				}
-			});
-		});
-
-		return () => {
-			unsubscribers.forEach((unsubscribe) => unsubscribe());
-		};
-	});
-};
+): Readable<Loadable<T>> =>
+	!Array.isArray(stores) ? single(stores, fn) : array(stores, fn as (values: Array<any>) => T);
