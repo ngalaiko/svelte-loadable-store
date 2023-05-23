6dd0c811866bd8ec781aa5634a2591d998ab755f
Nikita Galaiko <nikita@galaiko.rocks>
2023-05-23 15:47:16 +0200

    fix is... functions

--- src/types.ts
+++ src/types.ts
@@ -1,17 +1,26 @@
 export type Loading = { isLoading: true };
+export const Loading = {
+	isLoading: <T>(value: Loadable<T> | T): value is Loading =>
+		typeof value === 'object' &&
+		value !== null &&
+		Object.keys(value).length === 1 &&
+		'isLoading' in value &&
+		value.isLoading === true
+};
 
 export type Loaded<T> = { isLoading: false; value: T };
 export const Loaded = {
 	isLoaded: <T>(value: Loadable<T> | T): value is Loaded<T> =>
-		Loadable.isLoadable(value) && !value.isLoading
+		typeof value === 'object' &&
+		value !== null &&
+		Object.keys(value).length === 2 &&
+		'isLoading' in value &&
+		value.isLoading === false &&
+		'value' in value
 };
 
 export type Loadable<T> = Loading | Loaded<T>;
 export const Loadable = {
 	isLoadable: <T>(value: Loadable<T> | T): value is Loadable<T> =>
-		typeof value === 'object' &&
-		value !== null &&
-		Object.keys(value).length === 2 &&
-		'isLoading' in value &&
-		'value' in value
+		Loaded.isLoaded(value) || Loading.isLoading(value)
 };
