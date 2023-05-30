5e712644bed066787ca42fc87c29828ae3faf8fd
Nikita Galaiko <nikita@galaiko.rocks>
2023-05-30 16:00:11 +0200

    better types

--- src/utils.ts
+++ src/utils.ts
@@ -1,7 +1,7 @@
 import type { Readable } from 'svelte/store';
 import { type Loadable, Loading, Value } from './types';
 
-export const promisify = <T>(store: Readable<Loadable<T>>) =>
+export const promisify = <T>(store: Readable<Loadable<T>>): Promise<T> =>
 	new Promise((resovle, reject) => {
 		const unsubscribe = store.subscribe((value) => {
 			if (Loading.isLoading(value)) return;
